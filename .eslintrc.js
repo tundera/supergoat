5b27c3e07a3552623681ae48c16861b4e95213d2
tundera <tunderadev@gmail.com>
2021-08-27 19:10:19 -0600

    chore: add eslitnignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,62 @@
+import { join } from 'path'
+
+const cwd = process.cwd()
+
 /**
  * @type {import('eslint').Linter.Config}
  */
 module.exports = {
   root: true,
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:jest/recommended',
+    'plugin:jest-dom/recommended',
+    'plugin:jest-formatting/recommended',
+    'plugin:prettier/recommended',
   ],
-  plugins: ['@typescript-eslint', 'prettier', 'jest', 'jest-formatting'],
+  globals: {
+    context: 'readonly',
+    cy: 'readonly',
+    assert: 'readonly',
+    Cypress: 'readonly',
+    'jest/globals': true,
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'jest', 'jest-dom', 'jest-formatting'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    tsconfigRootDir: '.',
+    project: [
+      join(cwd, './tsconfig.base.json'),
+      join(cwd, './packages/*/tsconfig.json'),
+      join(cwd, './sites/*/tsconfig.json'),
+      join(cwd, './examples/*/tsconfig.json'),
+      join(cwd, './services/*/tsconfig.json'),
+      join(cwd, './scripts/tsconfig.json'),
+      join(cwd, './db/tsconfig.json'),
+      join(cwd, './tools/tsconfig.tools.json'),
+    ],
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  globals: {
-    context: 'readonly',
-    cy: 'readonly',
-    assert: 'readonly',
-    Cypress: 'readonly',
-  },
   rules: {
     'no-empty-function': 'off',
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-empty-function': [
       'error',
       { allow: ['private-constructors', 'decoratedFunctions'] },
     ],
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
   },
   overrides: [
     {
