de7c26370639b71801bc2ebebd241279e36c9a86
tundera <tunderadev@gmail.com>
2021-09-11 20:46:03 -0400

    chore: update gqty implementation

--- packages/theme/src/ThemeProvider/ThemeProvider.tsx
+++ packages/theme/src/ThemeProvider/ThemeProvider.tsx
@@ -1,22 +1,17 @@
 import type { FC } from 'react'
-import type { ChakraProviderProps } from '@chakra-ui/react'
+import type { ThemeProviderProps as ChakraProviderProps } from '@chakra-ui/react'
 
 import { cookieStorageManager, localStorageManager, ChakraProvider } from '@chakra-ui/react'
 
-import { theme as defaultTheme } from '../../src/theme'
+import { theme } from '../../src/theme'
 
 export type ServerSideProps<T> = { props: T } | Promise<{ props: T }>
 
-export interface ThemeProviderProps extends ChakraProviderProps {
+export interface ThemeProviderProps extends Omit<ChakraProviderProps, 'theme'> {
   cookies?: string
 }
 
-export const ThemeProvider: FC<ThemeProviderProps> = ({
-  cookies,
-  theme = defaultTheme,
-  children,
-  ...props
-}) => {
+export const ThemeProvider: FC<ThemeProviderProps> = ({ cookies, children, ...props }) => {
   return (
     <ChakraProvider
       {...props}
