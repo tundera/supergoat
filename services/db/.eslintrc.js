d709fedb60deea2853f680a78d7fbaffc39a7926
tundera <tunderadev@gmail.com>
2021-09-15 16:43:13 -0400

    chore: update structure

--- services/db/.eslintrc.js
+++ services/db/.eslintrc.js
@@ -2,6 +2,6 @@
  * @type {import('eslint').Linter.Config}
  */
 module.exports = {
-  extends: ['../.eslintrc.js', 'plugin:jsx-a11y/recommended'],
+  extends: ['../../.eslintrc.js', 'plugin:jsx-a11y/recommended'],
   overrides: [],
 }
